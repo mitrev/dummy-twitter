c70c91205d6e8f7c05517e003dd691a979994bbb
Strahil Mitrev <smitrev@sii.pl.eu>
2019-10-14 14:51:27 +0200

    init

---
+++ src/main/java/com/codechallenge/dummytwitter/DummytwitterApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.dummytwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummytwitterApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( DummytwitterApplication.class, args );
+    }
+
+}

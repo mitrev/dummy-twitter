2a756becdcd21ca4711b40347a6f84f0b526bc44
Strahil Mitrev <smitrev@sii.pl.eu>
2019-10-19 14:59:41 +0200

    Swagger added

--- src/main/java/com/codechallenge/dummytwitter/DummyTwitterApplication.java
+++ src/main/java/com/codechallenge/dummytwitter/DummyTwitterApplication.java
@@ -2,12 +2,14 @@ package com.codechallenge.dummytwitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class DummytwitterApplication {
+@EnableSwagger2
+public class DummyTwitterApplication {
 
     public static void main( String[] args ) {
-        SpringApplication.run( DummytwitterApplication.class, args );
+        SpringApplication.run( DummyTwitterApplication.class, args );
     }
 
 }
--- src/test/java/com/codechallenge/dummytwitter/DummyTwitterApplicationTests.java
+++ src/test/java/com/codechallenge/dummytwitter/DummyTwitterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DummytwitterApplicationTests {
+public class DummyTwitterApplicationTests {
 
     @Test
     public void contextLoads() {
